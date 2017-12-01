8d2bf25196dde95362ea97c6a5680083f4a50f3b
Seb <sweb1993@gmail.com>
2017-12-01 11:08:13 +0100

    no message

---
+++ Hertels Kataloger/WebApplication1/Models/UploaderViewModels.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class UploadModel
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
