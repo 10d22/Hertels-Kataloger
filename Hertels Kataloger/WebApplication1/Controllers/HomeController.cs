c7d5c38b440e811341eaf71c5cf42f28109b6e5e
Seb <sweb1993@gmail.com>
2017-12-01 11:01:29 +0100

    no message

---
+++ Hertels Kataloger/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*public ActionResult Index()
+        {
+            return View();
+        }
+        */
+        public ActionResult Katalog(string katalog) //   /Home/Katelog/?katelog=test
+        {
+            string test = katalog;
+            ViewBag.folder = test;
+
+            string path = HttpContext.Server.MapPath("~/Content/Uploads/" + test);
+            DirectoryInfo di = new DirectoryInfo(path);
+            int amountfiles = di.GetFiles().Length;
+            ViewBag.pages = amountfiles;
+
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            string path = HttpContext.Server.MapPath("~/Content/Uploads/");
+            var di = new DirectoryInfo(path);
+            var directories = di.EnumerateDirectories()
+                                .OrderBy(d => d.CreationTime)
+                                .Select(d => d.Name)
+                                .ToList();
+            /*string[] subfolders = System.IO.Directory.GetDirectories(path);*/
+
+            directories.Reverse();
+
+            ViewBag.folders = new List<string>();
+            foreach (string subfolder in directories)
+            {
+                if (subfolder != null)
+                {
+                    /*DirectoryInfo dir = new DirectoryInfo(subfolder);*/
+                    string folder = string.Format(subfolder);
+
+                    ViewBag.folders.Add(folder);
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Hertels Kataloger/WebApplication1/Controllers/UploaderController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    public class UploaderController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Uploader()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Delete(string katalog)
+        {
+            string foldername = katalog;
+            ViewBag.folder = foldername;
+
+            string path = Server.MapPath("~/Content/Uploads/" + katalog + "/");
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path, true);
+            }
+
+
+
+            return Redirect("~/Home/");
+        }
+
+
+        [HttpPost]
+        public ActionResult Uploader(List<HttpPostedFileBase> postedFiles, String name)
+        {
+            string path = Server.MapPath("~/Content/Uploads/" + name + "/");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+
+            foreach (HttpPostedFileBase postedFile in postedFiles)
+            {
+                if (postedFile != null)
+                {
+                    string fileName = Path.GetFileName(postedFile.FileName);
+                    postedFile.SaveAs(path + fileName);
+                    ViewBag.Message += string.Format("<b>{0}</b> uploaded.<br />", fileName);
+                }
+            }
+
+
+            return Redirect("~/Home/");
+        }
+    }
+}
